e96573b0d151273ce8297d6ba212bd328a045708
YehorVahin <davyjohns322@gmail.com>
2025-01-06 19:00:19 +0200

    Update send.php

--- php/send.php
+++ php/send.php
@@ -17,7 +17,13 @@ $type = trim($type);
 mail("yehor.vahin@gmail.com", "Website application", "Name:".$fio.", E-mail: ".$email.", Phone: ".$phone.", Type: ".$type.", From: example2@mail.ru \r\n");
 if (mail("yehor.vahin@gmail.com", "Website application", "Name:".$fio.", E-mail: ".$emailю.", Phone: ".$phone.", Type: ".$type.", From: example2@mail.ru \r\n"))
  {
-    echo "сообщение успешно отправлено";
+    function goback()
+{
+    header("Location: {$_SERVER['HTTP_REFERER']}");
+    exit;
+}
+  
+goback();
 } else {
     echo "при отправке сообщения возникли ошибки";
 }
