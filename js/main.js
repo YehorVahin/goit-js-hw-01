d7a5b53d55ec4d6fb479b4eb589f1dbd0109b110
YehorVahin <davyjohns322@gmail.com>
2025-01-10 15:35:46 +0200

    1

--- js/main.js
+++ js/main.js
@@ -1,210 +1,65 @@
-"use strict"
-
-// document.addEventListener('DOMContentLoaded', function(){
-//     const form = document.getElementById('form');
-//     form.addEventListener('submit', formSend);
-
-//     async function formSend(e) {
-//         e.preventDefault();
-
-//         let error = formValidate(form);
-
-//         let formData = new FormData(form);
-        
-
-//         if (error === 0) {
-//             form.classList.add('_sending');
-//             let response = await fetch('send.php', {
-//                 method: 'POST',
-//                 body: formData
-//             });
-//             if (response.ok) {
-//                 let result = await response.json();
-//                 alert(result.message);
-//                 formPreview.innerHTML = '';
-//                 form.reset();
-//                 form.classList.remove('_sending');
-//             }else{
-//                 alert("Error");
-//                 form.classList.remove('_sending');
-//             }
-//         }else{
-//             alert('Заполните обязательные поля');
-//         }
-//     }
-
-//     function formValidate(form){
-//         let error = 0;
-//         let formReq = document.querySelectorAll('._req');
-
-//         for (let index = 0; index < formReq.length; index++) {
-//             const input = formReq[index];
-//             formRemoveError(input);
-
-//             if (input.classList.contains('_email')) {
-//                 if (emailTest(input)) {
-//                     formAddError(input);
-//                     error++;
-//                 }
-//             }else if (input.getAttribute('type') === "checkbox" && input.checked === false){
-//                 formAddError(input);
-//                 error++;
-//             }else{
-//                 if (input.value === '') {
-//                     formAddError(input);
-//                     error++;
-//                 }
-//             }
-//         }
-//     }
-//     function formAddError(input){
-//         input.parentElement.classList.add('_error');
-//         input.classList.add('_error');
-//     }
-//     function formRemoveError(input) {
-//         input.parentElement.classList.remove('_error');
-//         input.classList.remove('_error');
-//     }
-//     function emailTest(input){
-//         return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-//     }
-// });
+"use strict";
 
 
+$(function () {
+  $(".slider__inner").slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    infinite: false,
+  });
+  $("select").styler();
+  $(".news__slider-inner").slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    infinite: false,
+  });
 
-
-$(function(){
-
-    $('.slider__inner').slick({
-        prevArrow:'<button type="button" class="slick-btn slick-prev"></button>',
-        nextArrow:'<button type="button" class="slick-btn slick-next"></button>',
-        infinite: false,
-    });
-    $('select').styler();
-    $('.news__slider-inner').slick({
-        prevArrow:'<button type="button" class="slick-btn slick-prev"></button>',
-        nextArrow:'<button type="button" class="slick-btn slick-next"></button>',
-        infinite: false,
-    });
-
-    $('.header__btn-menu').on('click', function(){
-        $('.menu ul').slideToggle();
-    });
+  $(".header__btn-menu").on("click", function () {
+    $(".menu ul").slideToggle();
+  });
 });
 
 
 
 
 
-const form = document.forms["form"]; // считываем форму
-const formArr = Array.from(form); // формируем массив из элементов формы
-const validFormArr = []; // в этом массиве хранятся поля, которые нужно проверить
-// const button = form.elements["input[type='submit']"]; // считываем кнопку
+const ERROR_MESSAGE = "Some Error!";
+const SUCCESS_MESSAGE = "Successfuly send!";
+const form = document.querySelector(".form");
+form.addEventListener("submit", formSend);
 
-formArr.forEach((el) => {
-    if (el.hasAttribute("data-reg")) {
-      el.setAttribute("is-valid", "0");
-      validFormArr.push(el);
-    }
+async function formSend(event) {
+  event.preventDefault();
+  const formData = new FormData(form);
+
+  let response = await fetch("php/send.php", {
+    method: "POST",
+    body: formData,
+    mode: 'no-cors'
   });
-  
-  form.addEventListener("input", inputHandler);
-  form.addEventListener("submit", formCheck);
-
-  function inputHandler({ target }) {
-    if (target.hasAttribute("data-reg")) {
-      inputCheck(target);
-    }
-  }
-  
-  function inputCheck(el) {
-    const inputValue = el.value;
-    const inputReg = el.getAttribute("data-reg");
-    const reg = new RegExp(inputReg);
-    if (reg.test(inputValue)) {
-      el.setAttribute("is-valid", "1");
-      el.style.border = "2px solid rgb(0, 196, 0)";
-    } else {
-      el.setAttribute("is-valid", "0");
-      el.style.border = "2px solid rgb(255, 0, 0)";
-    }
-  }
-  function formCheck(e) {
-    const allValid = [];
-    validFormArr.forEach((el) => {
-      allValid.push(el.getAttribute("is-valid"));
-    });
-    const isAllValid = allValid.reduce((acc, current) => {
-      return acc && current;
-    });
-  
-    if (!Boolean(Number(isAllValid))) {
-      e.preventDefault();
-    }
-  }
-  function formCheck(e) {
-    e.preventDefault(); // блокируем input
-    const allValid = []; // создаем массив валидных значений
-    validFormArr.forEach((el) => {
-      allValid.push(el.getAttribute("is-valid")); // проверяем каждое поле
-    });
-    const isAllValid = allValid.reduce((acc, current) => {
-      // проверяем, чтобы все было правильно
-      return acc && current;
-    });
-    if (!Boolean(Number(isAllValid))) {
-      alert("Заполните поля правильно!"); // если не правильно - сообщение пользователю
-      return;
-    }
-    formSubmit(); // если правильно - отправляем данные
-  }
-  async function formSubmit() {
-    const data = serializeForm(form); // получаем данные формы
-    const response = await sendData(data); // отправляем данные на почту
-    if (response.ok) {
-      let result = await response.json(); // если ответ OK отвечает пользователю
-      alert(result.message); // .. что данные отправлены
-      formReset(); // сбрасываем поля формы
-    } else {
-      alert("Код ошибки: " + response.status); // если not OK - показываем код ошибки
-    }
-  }
-  
-  function serializeForm(formNode) {
-    // формируем данные формы
-    return new FormData(form);
-  }
-  
-  async function formSubmit() {
-    const data = serializeForm(form); // получаем данные формы
-    const response = await sendData(data); // отправляем данные на почту
-    if (response.ok) {
-      let result = await response.json(); // если ответ OK отвечает пользователю
-      alert(result.message); // .. что данные отправлены
-      formReset(); // сбрасываем поля формы
-    } else {
-      alert("Код ошибки: " + response.status); // если not OK - показываем код ошибки
-    }
-  }
-  
-  function serializeForm(formNode) {
-    // формируем данные формы
-    return new FormData(form);
-  }
-  
-  async function sendData(data) {
-    return await fetch("send_mail.php", {
-      // отправляем в скрипт send_mail.php
-      method: "POST", // методом POST
-      body: data,
-    });
-  }
-  
-  function formReset() {
-    // сброс полей формы
+ 
+  if (response.ok) {
+    colorMessage = "linear-gradient(to right, #00b09b, #96c93d)";
+    showTostMessage(SUCCESS_MESSAGE, colorMessage);
     form.reset();
-    validFormArr.forEach((el) => {
-      el.setAttribute("is-valid", 0);
-      el.style.border = "none";
-    });
+  } else {
+    colorMessage = "linear-gradient(to right, red, red)";
+    showTostMessage(ERROR_MESSAGE, colorMessage);
   }
+}
+
+function showTostMessage(message, colorMessage) {
+  Toastify({
+    text: message,
+    duration: 5000,
+    newWindow: true,
+    className: "toast-message",
+    close: true,
+    gravity: "top",
+    position: "center",
+    stopOnFocus: true,
+    style: {
+      background: colorMessage,
+    },
+  }).showToast();
+}
--- php/send.php
+++ php/send.php
@@ -1,60 +1,70 @@
 <?php
- use PHPMailer\PHPMailer\PHPMailer;
- use PHPMailer\PHPMailer\Exception;
+// //Import PHPMailer classes into the global namespace
+// //These must be at the top of your script, not inside a function
+// use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\SMTP;
+// use PHPMailer\PHPMailer\Exception;
 
- require'PHPMailer/src/Exception.php';
- require'PHPMailer/src/PHPMailer.php';
+// require 'PHPMailer/src/Exception.php';
+// require 'PHPMailer/src/PHPMailer.php';
+// require 'PHPMailer/src/SMTP.php';
 
- $mail = new PHPMailer(true);
- $mail->CharSet = "UTF-8";
-//  $mail->setLanguage('eng', 'PHPMailer/language/');
- $mail->isHTML(true);
+// //Create an instance; passing `true` enables exceptions
+// $mail = new PHPMailer(true);
 
-$name = $_POST["name"];
-$email = $_POST["email"];
-$phone = $_POST["phone"];
-$type = $_POST["type"];
+// try {
+//     //Server settings
+//     $mail->isSMTP();                                          //Send using SMTP
+//     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+//     $mail->SMTPAuth   = true;                                 //Enable SMTP authentication
+//     $mail->Username   = '';                                   //SMTP username
+//     $mail->Password   = '';                                   //SMTP password
+//     $mail->SMTPSecure = 'TLS';                                //Enable implicit TLS encryption
+//     $mail->Port       = 587;                                  //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-$body = $name .''. $email .''. $phone .''. $type;
+//     //Recipients
+//     $mail->setFrom('davyjohns322@gmail.com', 'form');
+//     $mail->addAddress('yehor.vahin@gmail.com');     //Add a recipient
+//     $body = '<p>Email: '.$_POST['email'].'</p>' . '<p>User name: '.$_POST['name'].'</p>';
 
+//     //Content
+//     $mail->isHTML(true);                                  //Set email format to HTML
+//     $mail->CharSet = 'UTF-8';
+//     $mail->Subject = 'Message';
+//     $mail->Body    = $body;
 
+//     $mail->send();
+//     echo 'Message has been sent';
+// } catch (Exception $e) {
+//     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+// }
 
+use PHPMailer\PHPMailer\PHPMailer;
 
-
-
-
-
-
-//  $mail->setFrom('davyjohns322@gmail.com');
-  $mail->addAddress('yehor.vahin@gmail.com');
-  $mail->Subject = 'Новая заявка с сайта';
-
-//  $body = '<h1>Congratulations</h1>';
-
-//  if (trim(!empty($_POST['name']))) {
-//     $body.='<p><strong>E-mail:</strong> '.$_POST['name'].'</p>';
-//  }
-//  if (trim(!empty($_POST['phone']))) {
-//     $body.='<p><strong>E-mail:</strong> '.$_POST['phone'].'</p>';
-//  }
-//  if (trim(!empty($_POST['email']))) {
-//     $body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
-//  }
-//  if (trim(!empty($_POST['type']))) {
-//     $body.='<p><strong>E-mail:</strong> '.$_POST['type'].'</p>';
-//  }
-
-  $mail->Body = $body;
-
-  $mail->send();
-
-//  if (!$mail->send()) {
-//     $message = 'Error';
-//  }else {
-//     $message = 'Succesful';
-//  }
-//  $response = ['message' => $message];
-
-//  header('Content-type: application/json');
-//  echo json_encode($response);
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+ 
+$mail = new PHPMailer(true);
+$mail->isSMTP();
+$mail->Host = 'test.local';
+$mail->SMTPAuth = true;
+$mail->Username = 'davyjohns322@gmail.com';
+$mail->Password = '';
+$mail->SMTPSecure = 'tls';
+$mail->Port = 587;
+ 
+$mail->setFrom($email, $name);
+$mail->addAddress('yehor.vahin@gmail.com');
+$mail->isHTML(true);
+ 
+$mail->Subject = 'Сообщение с формы обратной связи';
+$mail->Body    = "От: $name <br> Email: $email <br> Сообщение: $phone";
+ 
+if(!$mail->send()) {
+    echo 'Ошибка при отправке сообщения.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Сообщение успешно отправлено!';
+}
 ?>
